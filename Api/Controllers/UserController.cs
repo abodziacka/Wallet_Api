52bb8ed9035ee319e510e7f9a8fc0f9a31e284d3
abodziacka <olabodziacka@op.pl>
2020-12-27 15:50:36 +0100

    init

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Options;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+        private readonly ApplicationSettings _appSettings;
+
+
+        public UserController(UserManager<User> userManager, SignInManager<User> signInManager, IOptions<ApplicationSettings> appSettings)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost]
+        [Route("registration")]
+        //POST: /user/registration
+
+        public async Task<Object> PostUser([FromBody] UserModel model)
+        {
+            var user = new User()
+            {
+                UserName = model.Username,
+                Email = model.Email
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, model.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        //POST: /user/login
+
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim("UserID", user.Id.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.JWT_Secret)), SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                var token = tokenHandler.WriteToken(securityToken);
+                return Ok(new { token });
+
+            }
+            else
+            {
+                return BadRequest(new { message = "Username or password incorect." });
+            }
+        }
+
+
+    }
+
+    
+
+}
---
+++ Api/Controllers/UserProfileController.cs
@@ -0,0 +1,36 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private UserManager<User> _userManager;
+        public UserProfileController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        [Authorize]
+        //GET: /UserProfile
+
+        public async Task<Object> GetUserProfile()
+        {
+            string userId = User.Claims.First(c => c.Type == "UserID").Value;
+            var user = await _userManager.FindByIdAsync(userId);
+            return new
+            {
+                user.Email,
+                user.UserName
+            };
+        }
+    }
+}
